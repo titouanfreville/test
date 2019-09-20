4b3bd018cd5c49ce4c25989aa0289e0b0fafd0b0
Titouan <tfreville@tankyou.co>
2019-09-20 16:14:57 +0200

    Add another test db

--- api.py
+++ api.py
@@ -4,7 +4,8 @@ from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///prod.db'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
