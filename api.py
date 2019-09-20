ddef3536944acbb43ec3c4873b04d25b80ea7643
Titouan <tfreville@tankyou.co>
2019-09-20 15:51:21 +0200

    Add migration

--- api.py
+++ api.py
@@ -13,3 +13,9 @@ migrate = Migrate(app, db)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
+
+
+class Spell(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    cost = db.Column(db.String(128), index=True)
