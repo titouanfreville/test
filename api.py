34c5491ace3ef605739fcdcdcff337da24712081
Titouan <tfreville@tankyou.co>
2019-09-20 15:49:55 +0200

    Add migration

--- api.py
+++ api.py
@@ -13,3 +13,9 @@ migrate = Migrate(app, db)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
+
+
+class Troll(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    fly = db.Column(db.String(128))
